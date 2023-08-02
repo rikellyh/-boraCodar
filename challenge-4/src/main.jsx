8512d2dc9e7242b86d883f7bf4ab5aa21c284016
Kelly <rikellyh898@gmail.com>
2023-08-02 17:20:30 -0300

    Using Chakra UI

--- challenge-4/src/main.jsx
+++ challenge-4/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Roboto",
+  },
+  colors: {
+    brand: {
+      white: "#E1E1E6",
+      green1: "#00B37E",
+      green2: "#07847E",
+      purple1: "#633BBC",
+      purple2: "#282843",
+      purple3: "#1A1924",
+    },
+  },
+  styles: {
+    global: {
+      "html, body": {
+        backgroundColor: "brand.purple3",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
