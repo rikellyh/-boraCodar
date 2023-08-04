cdabe78e5556e6cc0b3ffd841cf75b54e763fe11
Kelly <rikellyh898@gmail.com>
2023-08-04 14:13:33 -0300

    Finish Mobile Design

--- challenge-4/src/pages/RoomPage.jsx
+++ challenge-4/src/pages/RoomPage.jsx
@@ -7,8 +7,12 @@ import {
   VStack,
   HStack,
   Box,
+  Grid,
+  GridItem,
+  Input,
+  Button,
 } from "@chakra-ui/react";
-import { ArrowBackIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 import Avatar from "../assets/avatar.jpeg";
 import { useEffect, useState } from "react";
@@ -38,7 +42,7 @@ const RoomPage = () => {
 
   return (
     <>
-      <Container p="0" my="1rem">
+      <Container maxW="850px" p="0" my="1rem">
         <Flex
           boxShadow="0px 4px 8px rgba(0, 0, 0, 0.2)"
           alignItems="center"
@@ -78,30 +82,112 @@ const RoomPage = () => {
             </VStack>
           </HStack>
         </Flex>
-        <Box mt="1.5rem">
+        <VStack mt="1.5rem" gap="unset">
           <Text textAlign="center" fontSize="0.75rem" color="brand.white">
             {currentTime}
           </Text>
-          <Box w="50%" m="1rem">
-            <Box borderRadius="0 8px 8px 8px" p="0.5rem" bg="brand.purple1">
-              <Text fontSize="0.75rem" color="brand.white">
-                Eae, vamo ver Barbie hoje?
+          <Grid
+            w="90%"
+            h="100vh"
+            maxH="350px"
+            mt="1.25rem"
+            overflow="auto"
+            templateRows="repeat(2, 1fr)"
+          >
+            <GridItem maxW="10rem" m="0.5rem">
+              <Box borderRadius="0 8px 8px 8px" p="0.5rem" bg="brand.purple1">
+                <Text fontSize="0.75rem" color="brand.white">
+                  Eae, vamo ver Barbie hoje?
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
               </Text>
-            </Box>
-            <Text fontSize="0.6rem" color="brand.white" mt="5px">
-              {currentTime}
-            </Text>
-          </Box>
-          <Box w="50%" m="1rem">
-            <Box borderRadius="0 8px 8px 8px" p="0.5rem" bg="brand.green2">
-              <Text fontSize="0.75rem" color="brand.white">
-                Vou ver e te aviso
+            </GridItem>
+            <GridItem maxW="10rem" m="0.5rem" justifySelf="right">
+              <Box borderRadius="8px 8px 0 8px" p="0.5rem" bg="brand.green2">
+                <Text fontSize="0.75rem" color="brand.white">
+                  Talvez
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
               </Text>
-            </Box>
-            <Text fontSize="0.6rem" color="brand.white" mt="5px">
-              {currentTime}
-            </Text>
-          </Box>
+            </GridItem>
+            <GridItem maxW="10rem" m="0.5rem" justifySelf="right">
+              <Box borderRadius="8px 8px 0 8px" p="0.5rem" bg="brand.green2">
+                <Text fontSize="0.75rem" color="brand.white">
+                  Preciso ir ao mercado
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
+              </Text>
+            </GridItem>
+            <GridItem maxW="10rem" m="0.5rem" justifySelf="right">
+              <Box borderRadius="8px 8px 0 8px" p="0.5rem" bg="brand.green2">
+                <Text fontSize="0.75rem" color="brand.white">
+                  Dia de fazer feira 🥱
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
+              </Text>
+            </GridItem>
+            <GridItem maxW="10rem" m="0.5rem" justifySelf="right">
+              <Box borderRadius="8px 8px 0 8px" p="0.5rem" bg="brand.green2">
+                <Text fontSize="0.75rem" color="brand.white">
+                  Podemos ir amanhã?
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
+              </Text>
+            </GridItem>
+            <GridItem maxW="10rem" m="0.5rem" justifySelf="right">
+              <Box borderRadius="8px 8px 0 8px" p="0.5rem" bg="brand.green2">
+                <Text fontSize="0.75rem" color="brand.white">
+                  ?
+                </Text>
+              </Box>
+              <Text fontSize="0.6rem" color="brand.white" mt="5px">
+                {currentTime}
+              </Text>
+            </GridItem>
+          </Grid>
+        </VStack>
+        <Box
+          maxH="6.25rem"
+          w="90%"
+          mx="auto"
+          borderRadius="25px"
+          boxShadow="0px 0px 8px rgba(0, 0, 0, 0.2)"
+        >
+          <Input
+            w="86%"
+            placeholder="Digite sua mensagem"
+            variant="unstyled"
+            color="brand.white"
+            fontSize="0.75rem"
+            border="none"
+            outline="none"
+            padding="0.5rem 1rem"
+          />
+          <Button
+            w="1rem"
+            h="2.5rem"
+            bg="transparent"
+            border="none"
+            outline="none"
+            _hover={{
+              bg: "none",
+              color: "inherit",
+              textDecoration: "none",
+              boxShadow: "none",
+            }}
+          >
+            <ChevronRightIcon w="1.5rem" h="1.5rem" color="brand.white" />
+          </Button>
         </Box>
       </Container>
     </>
